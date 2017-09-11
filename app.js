b1039276e4ef13b325bee0e618e3380326f8f671
Xinyang Zhang <xinyzhang9@gmail.com>
2017-09-11 14:44:42 +0800

    pug

--- app.js
+++ app.js
@@ -7,11 +7,38 @@ app.set('views',path.join(__dirname, 'views'));
 app.set('view engine','pug');
 
 app.get('/',(req,res)=>{
+  let articles = [
+    {
+      id:1,
+      title: 'article 1',
+      author: 'author 1',
+      body: 'this is article 1'
+    },
+    {
+      id:2,
+      title: 'article 2',
+      author: 'author 2',
+      body: 'this is article 2'
+    },
+    {
+      id:3,
+      title: 'article 3',
+      author: 'author 3',
+      body: 'this is article 3'
+    }
+  ]
   res.render('index',{
-    title:'hi'
+    title:'Articles',
+    articles:articles
   });
 })
 
-app.listen(8888,()=>{
+app.get('/articles/add',(req,res)=>{
+  res.render('add_article',{
+    title: 'add article'
+  })
+})
+
+app.listen(8000,()=>{
   console.log('app running on localhost:8888  ')
 });
